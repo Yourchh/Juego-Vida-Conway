e75d7a0eb8e3a95d6a967015611795e7d6b7cae0
Jorge Hernandez <hu220111169@lahuerta.tecmm.edu.mx>
2026-05-08 00:21:07 -0600

    corrección de juego.js

--- js/juego.js
+++ js/juego.js
@@ -1,89 +1,143 @@
-var canvas, ctx;
+var canvas;
+var ctx;
 var fps = 30;
-var filas = 100, columnas = 100;
+
+var canvasX = 500;
+var canvasY = 500;
+var tileX, tileY;
+
 var tablero;
-var blanco = '#FFFFFF', negro = '#000000';
+var filas = 100;
+var columnas = 100;
 
-function inicializa() {
-    canvas = document.getElementById('pantalla');
-    ctx = canvas.getContext('2d');
-    
-    canvas.width = 500;
-    canvas.height = 500;
-
-    // Crear e inicializar tablero con agentes aleatorios
-    tablero = creaArray2D(filas, columnas);
-    for(let y=0; y<filas; y++){
-        for(let x=0; x<columnas; x++){
-            let estado = Math.floor(Math.random()*2);
-            tablero[y][x] = new Agente(x, y, estado);
-        }
-    }
-
-    // Una vez creados todos, asignar vecinos a cada uno
-    for(let y=0; y<filas; y++){
-        for(let x=0; x<columnas; x++){
-            tablero[y][x].addVecinos();
-        }
-    }
-
-    setInterval(function(){ principal(); }, 1000/fps);
-}
+var blanco = '#FFFFFF';
+var negro = '#000000';
 
 function creaArray2D(f, c) {
-    var obj = new Array(f);
-    for(let i=0; i<f; i++) obj[i] = new Array(c);
-    return obj;
+  var obj = new Array(f);
+  for (y = 0; y < f; y++) {
+    obj[y] = new Array(c);
+  }
+  return obj;
 }
 
-// Objeto Agente (Célula)
-function Agente(x, y, estado) {
-    this.x = x;
-    this.y = y;
-    this.estado = estado;
-    this.estadoProx = estado;
-    this.vecinos = [];
+var Agente = function(x, y, estado) {
+  this.x = x;
+  this.y = y;
+  this.estado = estado;
+  this.estadoProx = this.estado;
+  this.vecinos = [];
 
-    this.addVecinos = function() {
-        for(let i=-1; i<2; i++){
-            for(let j=-1; j<2; j++){
-                let xV = (x + j + columnas) % columnas;
-                let yV = (y + i + filas) % filas;
-                if(i !== 0 || j !== 0) this.vecinos.push(tablero[yV][xV]);
-            }
+  this.addVecinos = function() {
+    var xVecino;
+    var yVecino;
+
+    for (i = -1; i < 2; i++) {
+      for (j = -1; j < 2; j++) {
+        xVecino = (this.x + j + columnas) % columnas;
+        yVecino = (this.y + i + filas) % filas;
+
+        if (i != 0 || j != 0) {
+          this.vecinos.push(tablero[yVecino][xVecino]);
         }
-    };
+      }
+    }
+  }
 
-    this.dibuja = function() {
-        ctx.fillStyle = this.estado === 1 ? blanco : negro;
-        ctx.fillRect(this.x * 5, this.y * 5, 5, 5);
-    };
+  this.dibuja = function() {
+    var color;
+    if (this.estado == 1) {
+      color = blanco;
+    } else {
+      color = negro;
+    }
 
-    this.nuevoCiclo = function() {
-        let suma = 0;
-        for(let v of this.vecinos) suma += v.estado;
-        
-        this.estadoProx = this.estado; // Por defecto igual
-        if(suma < 2 || suma > 3) this.estadoProx = 0; // Muerte
-        if(suma === 3) this.estadoProx = 1; // Vida/Reproducción
-    };
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x * tileX, this.y * tileY, tileX, tileY);
+  }
 
-    this.mutacion = function() {
-        this.estado = this.estadoProx;
-    };
+  this.nuevoCiclo = function() {
+    var suma = 0;
+    for (i = 0; i < this.vecinos.length; i++) {
+      suma += this.vecinos[i].estado;
+    }
+
+    this.estadoProx = this.estado;
+
+    if (suma < 2 || suma > 3) {
+      this.estadoProx = 0;
+    }
+
+    if (suma == 3) {
+      this.estadoProx = 1;
+    }
+  }
+
+  this.mutacion = function() {
+    this.estado = this.estadoProx;
+  }
+}
+
+function inicializaTablero(obj) {
+  var estado;
+  for (y = 0; y < filas; y++) {
+    for (x = 0; x < columnas; x++) {
+      estado = Math.floor(Math.random() * 2);
+      obj[y][x] = new Agente(x, y, estado);
+    }
+  }
+
+  for (y = 0; y < filas; y++) {
+    for (x = 0; x < columnas; x++) {
+      obj[y][x].addVecinos();
+    }
+  }
+}
+
+function borraCanvas() {
+  canvas.width = canvas.width;
+  canvas.height = canvas.height;
+}
+
+function inicializa() {
+  canvas = document.getElementById('pantalla');
+  ctx = canvas.getContext('2d');
+
+  canvas.width = canvasX;
+  canvas.height = canvasY;
+
+  tileX = Math.floor(canvasX / filas);
+  tileY = Math.floor(canvasY / columnas);
+
+  tablero = creaArray2D(filas, columnas);
+  inicializaTablero(tablero);
+
+  setInterval(function() {
+    principal();
+  }, 1000 / fps);
+}
+
+function dibujaTablero(obj) {
+  for (y = 0; y < filas; y++) {
+    for (x = 0; x < columnas; x++) {
+      obj[y][x].dibuja();
+    }
+  }
+
+  for (y = 0; y < filas; y++) {
+    for (x = 0; x < columnas; x++) {
+      obj[y][x].nuevoCiclo();
+    }
+  }
+
+  for (y = 0; y < filas; y++) {
+    for (x = 0; x < columnas; x++) {
+      obj[y][x].mutacion();
+    }
+  }
 }
 
 function principal() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for(let y=0; y<filas; y++){
-        for(let x=0; x<columnas; x++){
-            tablero[y][x].dibuja();
-            tablero[y][x].nuevoCiclo();
-        }
-    }
-    for(let y=0; y<filas; y++){
-        for(let x=0; x<columnas; x++){
-            tablero[y][x].mutacion();
-        }
-    }
+  borraCanvas();
+  dibujaTablero(tablero);
 }
